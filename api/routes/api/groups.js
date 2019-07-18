f0c1f5e6134de5312ab62e9b6a9f0931db54e074
John Connerton <conjohnerton@gmail.com>
2019-07-17 20:55:51 -0400

    created groups router module

--- api/routes/api/groups.js
+++ api/routes/api/groups.js
@@ -1,3 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const authenticate = require("../../middleware/auth");
+
+// router.get()
+
+// TODO: create group method
+//       post bookmark to group method
+//
+//       add user to group??
+
+module.exports = router;
