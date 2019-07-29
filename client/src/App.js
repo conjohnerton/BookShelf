6044f83a106cf7e9350b371a45524ced5418aa87
John Connerton <conjohnerton@gmail.com>
2019-07-29 00:05:59 -0400

    started toying with axios calls to API

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: null,
+            groups: []
+        };
+    }
+
+    componentDidMount() {
+        this.getUsers();
+    }
+
+    getUsers = async () => {
+        try {
+            let res = await axios.get("/api/users");
+            console.log(res);
+            this.setState({ user: res.data[1].name, groups: res.data[1].groups });
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    render() {
+        const users = (
+            <h1>
+                {this.state.user} ||| {this.state.groups.map((g) => g.name)}
+            </h1>
+        );
+
+        return <div className="App">{users}</div>;
+    }
 }
 
 export default App;
